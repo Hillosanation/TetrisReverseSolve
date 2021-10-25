4e7c13c0f5c1d37c8eb56c0ffe191d3a7501e125
SaNoyGit <88579655+SaNoyGit@users.noreply.github.com>
2021-10-25 21:17:11 +0800

    Delete Misc directory

--- Misc/LogError.cpp
+++
@@ -1,8 +0,0 @@
-#include "LogError.h"
-#include <iostream>
-
-void LogError::Log(std::string reason, std::string origin) {
-	std::cout << "Error in " + origin + " | Reason: " + reason << std::endl;
-}
-
-LogError::~LogError() {};
--- Misc/LogError.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-class LogError {
-public:
-	void Log(std::string reason, std::string origin);
-
-	~LogError();
-};
