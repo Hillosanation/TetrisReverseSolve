e49dbc6a8ee0796b0c7efc8cc928cf18041a231e
SaNoyGit <88579655+SaNoyGit@users.noreply.github.com>
2021-10-25 20:44:34 +0800

    Update and rename CommonDataTypes.h to Misc/CommonDataTypes.h

--- Misc/CommonDataTypes.h
+++ Misc/CommonDataTypes.h
@@ -15,3 +15,16 @@ struct PFFSol { //playfieldfilled + solutions (pieces in order)
 	PlayFieldFilled PFFilled;
 	std::vector<PresentPieces> Solves = {};
 };
+
+struct StrSol { //general struct, encompassing both structs
+	std::string Str = "";
+	std::vector<PresentPieces> Solves = {};
+
+	StrSol() {};
+	StrSol(const FuSol& FSol) : Str(FSol.Fumen), Solves(FSol.Solves) {};
+};
+
+struct StrFieldStrPiece { //even more general, pieces are not converted to nested array of integers immediately
+	std::string Field;
+	std::vector<std::string> Solves = {};
+};
