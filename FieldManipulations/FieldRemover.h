6b8c6507d74d29f6c5555fac35cc4569bcad4ce0
SaNoyGit <88579655+SaNoyGit@users.noreply.github.com>
2021-11-02 18:41:47 +0800

    Update FieldRemover.h

--- FieldManipulations/FieldRemover.h
+++ FieldManipulations/FieldRemover.h
@@ -5,6 +5,7 @@
 #include "../cache/DataCacher.h"
 #include "FieldMatcher.h"
 #include "FieldMerger.h"
+#include "../SettingsData.h"
 class FieldRemover {
 	FieldMatcher Matcher;
 	FieldMerger Merger;
@@ -21,4 +22,5 @@ public:
 
 	std::vector<PFFSol> ReturnResults_2(const std::vector<PFFSol>& PFFSols, int depth);
 
+	FieldRemover(SettingsData& settings);
 };
