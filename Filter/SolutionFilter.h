a89b525fcb02c0e2b4fe4a75a3098429d6ecd65f
SaNoyGit <88579655+SaNoyGit@users.noreply.github.com>
2021-11-02 22:18:57 +0800

    Update SolutionFilter.h

--- Filter/SolutionFilter.h
+++ Filter/SolutionFilter.h
@@ -27,8 +27,8 @@ public:
 	std::vector<PFFSol> Filter(const std::vector<PFFSol>& PFFSols, SettingsData& Settings) {
 		int MinimumNum = std::stoi(Settings.GetValue(SettingsData::SettingsEnum::COUNT_MIN_FILTER));
 		auto StartTime = std::chrono::system_clock::now();
-		std::vector<PFFSol> Output;
-		if (MinimumNum != 1) return Filter_MinimumSolutions(PFFSols, MinimumNum);
+		std::vector<PFFSol> Output = PFFSols;
+		if (MinimumNum != 1) Output = Filter_MinimumSolutions(Output, MinimumNum);
 		
 		auto EndTime = std::chrono::system_clock::now();
 		std::cout << "Time taken for filter: " << std::chrono::duration_cast<std::chrono::microseconds>(EndTime - StartTime).count() / 1000.0 << "ms\n";
